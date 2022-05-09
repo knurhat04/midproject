059bb9fc1037a11b9c2240cd026edaaa55596ecf
knurhat04 <98089455+knurhat04@users.noreply.github.com>
2022-05-10 00:17:22 +0600

    Updated server.js

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
 const port = 2000;
 app.use(express.static("public"))
 app.set('views', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Databse Connected Successfully!!");
+}).catch(err => {
+    console.log('Could not connect to the database', err);
+    process.exit();
+});
 
+app.get('/', (req, res) => {
+    res.json({"message": "Hello Crud Node Express"});
+});
+
+// const map = new mapgl.Map('container', {
+//     key: 'Your API access key',
+//     center: [55.31878, 25.23584],
+//     zoom: 13,
+// });
 
 app.use("/", require("./routes/index"));
 app.use("/men", require("./routes/men"));
@@ -12,9 +35,12 @@ app.use("/women", require("./routes/women"));
 app.use("/collection", require("./routes/collection"));
 app.use("/index", require("./routes/index"));
 
-
-
-
+const UserRoute = require('./routes/UserRoute')
+const CartRoute = require('./routes/CartRoute')
+const OrderRoute = require('./routes/OrderRoute')
+app.use('/user',UserRoute)
+app.use('/order',OrderRoute)
+app.use('/cart',CartRoute)
 
 app.listen(port, () =>
     console.log(`App listening at http://localhost:${port}`)
